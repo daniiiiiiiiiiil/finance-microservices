def5e0bf3d68efd8d579ba90c837658b6d3f14c7
Danil <napalkov-danil@list.ru>
2026-08-29 18:20:15 +0300

    вернул старый просто с игнорированием, но все продолжает рабоатть

--- currency-service/pkg/grpcutil/interceptors/tracer.go
+++ currency-service/pkg/grpcutil/interceptors/tracer.go
@@ -2,9 +2,10 @@ package interceptors
 
 import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
-	"google.golang.org/grpc/stats"
+	"google.golang.org/grpc"
 )
 
-func TraceInterceptor() stats.Handler {
-	return otelgrpc.NewServerHandler()
+//nolint:staticcheck // SA1019: UnaryServerInterceptor is deprecated but still functional
+func TraceInterceptor() grpc.UnaryServerInterceptor {
+	return otelgrpc.UnaryServerInterceptor()
 }
